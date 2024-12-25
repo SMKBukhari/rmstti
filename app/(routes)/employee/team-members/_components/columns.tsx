d670659a9dbdabdb36e303d9ca5538b592fc6829
SMKBukhari <s.mkb2524@outlook.com>
2024-12-25 20:30:08 +0500

    updated some Employee Functionality

---
+++ app/(routes)/employee/team-members/_components/columns.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import CellActions from "./CellActions";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UserProfile } from "@prisma/client";
+
+export type ApplicantsColumns = {
+  user: UserProfile | null;
+  id: string;
+  fullName: string;
+  email: string;
+  contact: string;
+  appliedAt: string;
+  userImage: string;
+  designation: string;
+};
+
+export const columns: ColumnDef<ApplicantsColumns>[] = [
+  {
+    accessorKey: "userImage",
+    header: "",
+    cell: ({ row }) => {
+      const { userImage, fullName } = row.original;
+      return (
+        <Avatar className='w-10 h-10'>
+          <AvatarImage src={userImage} alt={fullName} />
+          <AvatarFallback>{fullName.charAt(0)}</AvatarFallback>
+        </Avatar>
+      );
+    },
+  },
+  {
+    accessorKey: "fullName",
+    header: "Full Name",
+    cell: ({ row }) => {
+      const { fullName } = row.original;
+      return (
+        <Link href={`/admin/applicants/${row.original.id}`}>{fullName}</Link>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "contact",
+    header: "Contact",
+  },
+  {
+    accessorKey: "designation",
+    header: "Designation",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { user, id, fullName, email } = row.original;
+      return (
+        id !== user?.userId && (
+          <CellActions user={user} id={id} fullName={fullName} email={email} />
+        )
+      );
+    },
+  },
+];
---
+++ app/(routes)/employee/team-members/page.tsx
@@ -0,0 +1,76 @@
+import CustomBreadCrumb from "@/components/CustomBreadCrumb";
+import { DataTable } from "@/components/ui/data-table";
+import { db } from "@/lib/db";
+import React from "react";
+import { ApplicantsColumns, columns } from "./_components/columns";
+import { format } from "date-fns";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const TeamMembersPage = async () => {
+  const cookieStore = cookies();
+  const userId = (await cookieStore).get("userId")?.value;
+
+  if (!userId) {
+    redirect("/signIn");
+  }
+
+  const user = await db.userProfile.findUnique({
+    where: {
+      userId: userId,
+    },
+    include: {
+      department: {
+        include: {
+          users: {
+            include: {
+              role: true,
+            },
+          },
+        },
+      },
+      role: true,
+    },
+  });
+
+  const teamMembers =
+    user?.role?.name !== "User"
+      ? user?.department?.users.filter((teamMember) => teamMember.userId) ?? []
+      : [];
+
+  // Formatting the applicants data for the table
+  const formattedApplicants: ApplicantsColumns[] = teamMembers.map(
+    (teamMember) => ({
+      user: user,
+      id: teamMember.userId,
+      fullName: teamMember.fullName ?? "N/A",
+      email: teamMember.email ?? "N/A",
+      contact: teamMember.contactNumber ?? "N/A",
+      appliedAt: teamMember.updatedAt
+        ? format(new Date(teamMember.updatedAt), "MMMM do, yyyy")
+        : "N/A",
+      designation: teamMember.designation ?? "N/A",
+      userImage: teamMember.userImage ?? "N/A",
+    })
+  );
+
+  return (
+    <div className='flex-col p-4 md:p-8 items-center justify-center flex'>
+      <div className='flex items-center justify-between w-full'>
+        <CustomBreadCrumb breadCrumbPage='Team Members' />
+      </div>
+
+      <div className='mt-6 w-full'>
+        <DataTable
+          columns={columns}
+          data={formattedApplicants}
+          searchKey='fullName'
+          routePrefix='manager/team-members'
+          userId={user?.userId}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TeamMembersPage;
